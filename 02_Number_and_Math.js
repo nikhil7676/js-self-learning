b9ca2ad2b916df5773dac14d6521e76332f33460
nikhil7676 <delllaptop9211@gmail.com>
2024-12-01 09:47:01 +0000

    number and Math-library basics function and methods

---
+++ 02_Number_and_Math.js
@@ -0,0 +1,25 @@
+//Number:
+
+const mobileNumber = 9429288221;
+console.log(mobileNumber);
+console.log(`your mobile number is : ${mobileNumber}`);
+
+//convert it into a string
+console.log(`mobile number after convert in string : ${mobileNumber.toString()}`)
+
+//used to show the values after the "."
+const balance = 100
+const balance2 = 10000
+console.log(balance.toFixed(2));
+
+//used to show the value in indian counting currancy way or numaric way
+// console.log(balance2.toLocaleString(en-IN)); OUTPUT : 10,000
+
+
+//............... Math ...................
+
+//round off the value
+console.log(Math.round(100.26));
+console.log(Math.round(100.62));
+
+console.log(Math.random()) // give random values between 0 to 9
