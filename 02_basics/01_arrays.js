50b6d8ea4d0c89a0257c60c36f938e113583d3a9
nikhil7676 <delllaptop9211@gmail.com>
2024-12-03 22:51:26 +0530

    array comman methods and spread operator

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+
+const bollywood_movies = ["salaar", "pushpa", "bahubali"]
+const hollywood_movies = ["deadpool", "iron-man", "etarnals"]
+
+// console.log(bollywood_movies);
+// console.log(hollywood_movies);
+
+// bollywood_movies.push("golmaal")
+// console.log(bollywood_movies);
+
+// bollywood_movies.pop()
+// console.log(bollywood_movies);
+
+// console.log(bollywood_movies + hollywood_movies)
+// console.log(typeof (bollywood_movies + hollywood_movies))
+
+// const all_movies = bollywood_movies.concat(hollywood_movies)
+// console.log(all_movies);
+
+//mostly used this "Spread opearator" in industry for concate or merge an array
+// console.log([...bollywood_movies,...hollywood_movies])
+
+// let multidimantional_array = [1,2,3,4,[5,6],7,8,[9,10,[11,12]]]
+// console.log(multidimantional_array.flat(Infinity))
+
+const demoArray = [1,2,3,4,5,6,7,8,9,10]
+console.log(demoArray.join().split(","))
+console.log(demoArray.split(","));
+
+
+console.log(Array.isArray(bollywood_movies));
+
+// console.log(Array.from(demoArray.join().split(",")))
+
